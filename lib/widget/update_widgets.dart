db83b5c36f5aa8ccfc75747b9e978ce2756ebf2e
Deepak <deepak.kumar@softradix.in>
2023-03-16 18:30:26 +0530

    update

---
+++ lib/widget/update_widgets.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart';
+import 'package:rfw/rfw.dart';
+
+Future<bool> updateWidgets(
+    {required String routeName,
+    required Runtime runtime,
+    required String urlPrefix}) async {
+  bool isCustomUi = false;
+
+  final Directory home = await getApplicationSupportDirectory();
+  final File settingsFile = File(path.join(home.path, 'settings.txt'));
+  final File currentFile = File(path.join(home.path, 'current.rfw'));
+
+  if (currentFile.existsSync()) {
+    try {
+      runtime.update(const LibraryName(<String>['main']),
+          decodeLibraryBlob(await currentFile.readAsBytes()));
+      isCustomUi = true;
+    } catch (e, stack) {
+      FlutterError.reportError(FlutterErrorDetails(exception: e, stack: stack));
+    }
+  }
+
+  final HttpClientResponse client =
+      await (await HttpClient().getUrl(Uri.parse('$urlPrefix/$routeName')))
+          .close();
+  await currentFile
+      .writeAsBytes(await client.expand((List<int> chunk) => chunk).toList());
+  await settingsFile.writeAsString(routeName);
+
+  return isCustomUi;
+}
